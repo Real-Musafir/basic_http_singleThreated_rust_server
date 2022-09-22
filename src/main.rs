d0cb4b115d60beb01a641d2a4b2358f67169e44e
Real-Musafir <alshahadath@gmail.com>
2022-09-22 18:57:54 +0600

    run html file with browser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,10 +16,15 @@ fn main() {
 fn handle_connection(mut stream: TcpStream){
 
     let mut buffer = [0;1024];
-
     stream.read(&mut buffer).unwrap(); //read is come from prelude thant included in above
 
-   let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("index.html").unwrap();
+
+   let response = format!(
+    "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+    contents.len(),
+    contents
+   ) ;
    stream.write(response.as_bytes()).unwrap();
    stream.flush().unwrap();
 }
